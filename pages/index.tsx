df631164a94a059bb7f184dedb77398fdb3cc7d8
Masanori Tani <metro@uta8a.org>
2022-02-27 15:05:53 +0900

    chore: display info
    Co-authored-by: Tksi <Tksi@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import type { NextPage } from 'next';
-import { Sample } from '@/components/Sample';
 import { getKioq } from 'utils/getKioq';
 import type { GetServerSideProps } from 'next';
 import type { Kioq } from '@/types/kioq';
+import { useState } from 'react';
+import type { ChangeEventHandler, MouseEventHandler } from 'react';
 
 export const getServerSideProps: GetServerSideProps<{
   kioq: Kioq;
@@ -12,16 +13,47 @@ export const getServerSideProps: GetServerSideProps<{
 };
 
 const Home: NextPage<{ kioq: Kioq }> = ({ kioq }) => {
+  const [tag, setTag] = useState(kioq[0].tag);
+  const [index, setIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const tagChangeHandler: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    setTag(event.target.value);
+    setIndex(0);
+  };
+  const indexChangeHandler: MouseEventHandler<HTMLButtonElement> = () => {
+    if (!isOpen) {
+      setIsOpen(true);
+      return;
+    }
+    if (index + 1 >= kioq.filter((quiz) => quiz.tag === tag).length) {
+      // index over kioq length, end of quiz
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
   return (
     <div>
       <Head>
         <title>kioq</title>
       </Head>
-
       <main>
         <p>kioq - simple flash card use Notion.</p>
-        <Sample />
-        <p>{JSON.stringify(kioq)}</p>
+        <select value={tag} onChange={tagChangeHandler}>
+          {Array.from(new Set(kioq.map((quiz) => quiz.tag))).map((tag) => {
+            return (
+              <option value={tag} key={tag}>
+                {tag}
+              </option>
+            );
+          })}
+        </select>
+        <div>
+          Quiz {kioq.filter((quiz) => quiz.tag === tag)[index]?.question}
+        </div>
+
+        <div>Ans {kioq.filter((quiz) => quiz.tag === tag)[index]?.answer}</div>
+        <button onClick={indexChangeHandler}>次へ</button>
       </main>
     </div>
   );
